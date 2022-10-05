20055b71139d1adaa491acab72771364b7ec9868
unknown <priyanka@tutedude.com>
2022-10-05 19:37:07 +0530

    sub link

--- routes/submission.js
+++ routes/submission.js
@@ -1,4 +1,4 @@
-const { resubmit, deleteFile, deleteLink } = require("../controllers/student");
+const { resubmit, deleteFile, deleteLink, resetText } = require("../controllers/student");
 
 const router = require("express").Router();
 
@@ -15,6 +15,8 @@ router.post("/resubmit",resubmit);
 router.delete("/submission/file",deleteFile);
 
 router.delete("/submission/link",deleteLink);
+
+router.put("/submission/text",resetText);
 
 // router.put("/attempt");
 
