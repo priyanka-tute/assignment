423061236d51067b8db0360abc00c0f20cfc2be1
unknown <priyanka@tutedude.com>
2023-02-07 20:01:05 +0530

    mentor login

--- services/mentor.js
+++ services/mentor.js
@@ -3,10 +3,14 @@ const Instructor = require("../models/Instructor");
 exports.validateMentor = (email,password) => {
     return new Promise((resolve,reject)=>{
     Instructor.find({email,password}).then((data)=>{
+        console.log("ins = ",data);
         if(data.length==0)
         throw "Invalid";
         else
-        resolve(data[0]["_id"]);
+        {
+            console.log("sending = ",data[0]["_id"]);
+            resolve(data[0]["_id"]);
+        }
     }).catch((err)=>{
         reject(err);
     })
