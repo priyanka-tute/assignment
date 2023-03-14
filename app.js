ca0f9de6f6891bb9d4715ba2bbec2e5e43c58f10
unknown <priyanka@tutedude.com>
2023-03-14 20:41:01 +0530

    cors

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ const {
 var app = express();
 app.use(cors());
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "upskill.tutedude.com"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
